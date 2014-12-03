0957c3a36badd0005c81d6fcbcdc70f4d262380c
Justin Espedal <justin.espedal@gmail.com>
2014-12-02 20:29:46 -0800

    Added scene switching, mobile support, touch controls to sample game.

---
+++ Dialog Extension/samples/Dialog Extension Sample/behavior-hx/Design_6_6_TouchControl.hx
@@ -0,0 +1,149 @@
+package scripts;
+
+import com.stencyl.graphics.G;
+import com.stencyl.graphics.BitmapWrapper;
+
+import com.stencyl.behavior.Script;
+import com.stencyl.behavior.ActorScript;
+import com.stencyl.behavior.SceneScript;
+import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.Key;
+import com.stencyl.utils.Utils;
+
+import nme.ui.Mouse;
+import nme.display.Graphics;
+import nme.display.BlendMode;
+import nme.display.BitmapData;
+import nme.display.Bitmap;
+import nme.events.Event;
+import nme.events.KeyboardEvent;
+import nme.events.TouchEvent;
+import nme.net.URLLoader;
+
+import box2D.common.math.B2Vec2;
+import box2D.dynamics.B2Body;
+import box2D.dynamics.B2Fixture;
+import box2D.dynamics.joints.B2Joint;
+
+import motion.Actuate;
+import motion.easing.Back;
+import motion.easing.Cubic;
+import motion.easing.Elastic;
+import motion.easing.Expo;
+import motion.easing.Linear;
+import motion.easing.Quad;
+import motion.easing.Quart;
+import motion.easing.Quint;
+import motion.easing.Sine;
+
+import com.stencyl.graphics.shaders.BasicShader;
+import com.stencyl.graphics.shaders.GrayscaleShader;
+import com.stencyl.graphics.shaders.SepiaShader;
+import com.stencyl.graphics.shaders.InvertShader;
+import com.stencyl.graphics.shaders.GrainShader;
+import com.stencyl.graphics.shaders.ExternalShader;
+import com.stencyl.graphics.shaders.InlineShader;
+import com.stencyl.graphics.shaders.BlurShader;
+import com.stencyl.graphics.shaders.SharpenShader;
+import com.stencyl.graphics.shaders.ScanlineShader;
+import com.stencyl.graphics.shaders.CSBShader;
+import com.stencyl.graphics.shaders.HueShader;
+import com.stencyl.graphics.shaders.TintShader;
+import com.stencyl.graphics.shaders.BloomShader;
+
+
+
+class Design_6_6_TouchControl extends ActorScript
+{          	
+	
+public var _control:String;
+
+public var _ctrl:String;
+
+public var _down:Bool;
+
+ 
+ 	public function new(dummy:Int, actor:Actor, engine:Engine)
+	{
+		super(actor, engine);	
+		nameMap.set("control", "_control");
+_control = "";
+nameMap.set("ctrl", "_ctrl");
+nameMap.set("down", "_down");
+_down = false;
+nameMap.set("Actor", "actor");
+
+	}
+	
+	override public function init()
+	{
+		    
+/* ======================== When Creating ========================= */
+        actor.setAnimation("" + ("" + _control));
+        _ctrl = _control;
+        _down = false;
+propertyChanged("_down", _down);
+        if(!(#if mobile true #else false #end))
+{
+            recycleActor(actor);
+}
+
+    
+/* =========================== On Actor =========================== */
+addMouseOverActorListener(actor, function(mouseState:Int, list:Array<Dynamic>):Void
+{
+if(wrapper.enabled && 3 == mouseState)
+{
+        simulateKeyPress(_ctrl);
+        _down = true;
+propertyChanged("_down", _down);
+}
+});
+    
+/* ============================ Click ============================= */
+addMouseReleasedListener(function(list:Array<Dynamic>):Void
+{
+if(wrapper.enabled)
+{
+        if(_down)
+{
+            _down = false;
+propertyChanged("_down", _down);
+            simulateKeyRelease(_ctrl);
+}
+
+}
+});
+    
+/* =========================== On Actor =========================== */
+addMouseOverActorListener(actor, function(mouseState:Int, list:Array<Dynamic>):Void
+{
+if(wrapper.enabled && 5 == mouseState)
+{
+        simulateKeyRelease(_ctrl);
+        _down = false;
+propertyChanged("_down", _down);
+}
+});
+
+	}	      	
+	
+	override public function forwardMessage(msg:String)
+	{
+		
+	}
+}
---
+++ Dialog Extension/samples/Dialog Extension Sample/behavior-hx/SceneEvents_1.hx
@@ -0,0 +1,90 @@
+package scripts;
+
+import com.stencyl.graphics.G;
+import com.stencyl.graphics.BitmapWrapper;
+
+import com.stencyl.behavior.Script;
+import com.stencyl.behavior.ActorScript;
+import com.stencyl.behavior.SceneScript;
+import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.Key;
+import com.stencyl.utils.Utils;
+
+import nme.ui.Mouse;
+import nme.display.Graphics;
+import nme.display.BlendMode;
+import nme.display.BitmapData;
+import nme.display.Bitmap;
+import nme.events.Event;
+import nme.events.KeyboardEvent;
+import nme.events.TouchEvent;
+import nme.net.URLLoader;
+
+import box2D.common.math.B2Vec2;
+import box2D.dynamics.B2Body;
+import box2D.dynamics.B2Fixture;
+import box2D.dynamics.joints.B2Joint;
+import box2D.collision.shapes.B2Shape;
+
+import motion.Actuate;
+import motion.easing.Back;
+import motion.easing.Cubic;
+import motion.easing.Elastic;
+import motion.easing.Expo;
+import motion.easing.Linear;
+import motion.easing.Quad;
+import motion.easing.Quart;
+import motion.easing.Quint;
+import motion.easing.Sine;
+
+import com.stencyl.graphics.shaders.BasicShader;
+import com.stencyl.graphics.shaders.GrayscaleShader;
+import com.stencyl.graphics.shaders.SepiaShader;
+import com.stencyl.graphics.shaders.InvertShader;
+import com.stencyl.graphics.shaders.GrainShader;
+import com.stencyl.graphics.shaders.ExternalShader;
+import com.stencyl.graphics.shaders.InlineShader;
+import com.stencyl.graphics.shaders.BlurShader;
+import com.stencyl.graphics.shaders.SharpenShader;
+import com.stencyl.graphics.shaders.ScanlineShader;
+import com.stencyl.graphics.shaders.CSBShader;
+import com.stencyl.graphics.shaders.HueShader;
+import com.stencyl.graphics.shaders.TintShader;
+import com.stencyl.graphics.shaders.BloomShader;
+
+
+
+class SceneEvents_1 extends SceneScript
+{          	
+	
+ 
+ 	public function new(dummy:Int, engine:Engine)
+	{
+		super(engine);
+		
+	}
+	
+	override public function init()
+	{
+		
+	}	      	
+	
+	override public function forwardMessage(msg:String)
+	{
+		
+	}
+}
