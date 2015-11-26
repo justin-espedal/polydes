93dfc1184ba45b6c8719ba9fe364125c39ab5e6c
Justin Espedal <justin.espedal@gmail.com>
2015-11-26 03:31:09 -0800

    Update DefaultNodeCreator.

--- Common/src/com/polydes/common/nodes/DefaultNodeCreator.java
+++ Common/src/com/polydes/common/nodes/DefaultNodeCreator.java
@@ -1,18 +1,43 @@
 package com.polydes.common.nodes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class DefaultNodeCreator<T extends Leaf<T,U>, U extends Branch<T,U>> implements NodeCreator<T,U>
 {
+	@Override
+	public T createNode(CreatableNodeInfo selected, String nodeName)
+	{
+		return null;
+	}
+	
 	@Override
 	public void editNode(T toEdit)
 	{
 	}
 	
 	@Override
+	public ArrayList<CreatableNodeInfo> getCreatableNodeList(U creationBranch)
+	{
+		return null;
+	}
+	
+	@Override
+	public ArrayList<NodeAction<T>> getNodeActions(T[] targets)
+	{
+		return null;
+	}
+	
+	@Override
 	public boolean attemptRemove(List<T> toRemove)
 	{
-		return true;
+		return false;
+	}
+	
+	@Override
+	public void nodeRemoved(T toRemove)
+	{
+		
 	}
 	
 	//TODO: Do we need complete uniqueness of item names?
